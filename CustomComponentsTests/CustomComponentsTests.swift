729fd82727c056249dd2824e0d3c5f97e6d8a8c2
Shoaib Ahmed Qureshi <08bitsaqureshi@seecs.edu.pk>
2019-04-03 22:30:20 +0500

    Initial Commit

---
+++ CustomComponentsTests/CustomComponentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  CustomComponentsTests.swift
+//  CustomComponentsTests
+//
+//  Created by Shoaib Ahmed Qureshi on 4/3/19.
+//  Copyright © 2019 Shoaib Ahmed Qureshi. All rights reserved.
+//
+
+import XCTest
+@testable import CustomComponents
+
+class CustomComponentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
